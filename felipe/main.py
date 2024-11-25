dbb08f04dfa835383c40adf69fd84fae1cf1fe43
Bruno Rodrigues <bruno197@outlook.pt>
2024-11-25 19:02:39 -0300

    Update main.py

--- felipe/main.py
+++ felipe/main.py
@@ -9,4 +9,5 @@ usuarios = ["Alice", "Bob", "Carlos", "Diana", "Elisa"]
 # 3. Verifique se o nome está na lista de usuários usando uma condicional (if/else).
 # 4. Imprima uma mensagem dizendo "Acesso permitido" se o nome estiver na lista, ou "Acesso negado" caso contrário.
 
-
+# usuarios = [("Alice", "Silva"), ("Bob", "Souza"), ("Carlos", "Oliveira"), ("Diana", "Martins"), ("Elisa", "Costa")]
+#Para a segunda parte do desafio, remove o # da linha 12 e coloque na linha 4
