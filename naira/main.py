90b1d98dede0f8667bd6d8f19ac711d14ee75477
Bruno Ferreira Rodrigues <bruno197@outlook.pt>
2024-11-25 22:03:09 -0300

    Add folders

---
+++ naira/main.py
@@ -0,0 +1,13 @@
+# Exercício para praticar:
+# Lista de usuários cadastrados
+
+usuarios = ["Alice", "Bob", "Carlos", "Diana", "Elisa"]
+
+# 2. Peça ao usuário que digite seu nome.
+# Dica: Você pode usar o método `input()` para receber dados do usuário.
+
+# 3. Verifique se o nome está na lista de usuários usando uma condicional (if/else).
+# 4. Imprima uma mensagem dizendo "Acesso permitido" se o nome estiver na lista, ou "Acesso negado" caso contrário.
+
+# usuarios = [("Alice", "Silva"), ("Bob", "Souza"), ("Carlos", "Oliveira"), ("Diana", "Martins"), ("Elisa", "Costa")]
+#Para a segunda parte do desafio, remove o # da linha 12 e coloque na linha 4
